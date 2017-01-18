9a1c5999841b1a9d5cf538268cf02e81d164de43
Elisabetta Iavarone <elisabetta.iavarone@epfl.ch>
2017-01-18 18:33:11 +0100

    Typo in setup.py script

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='BluePyEfe',
     version='0.1dev',
-    packages=['bluepyefe,],
+    packages=['bluepyefe'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
