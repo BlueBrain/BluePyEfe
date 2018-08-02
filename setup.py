91ea6c4a8ac4d8dc6247241d458c94ceace30ace
Benoît Coste <benoit.coste@epfl.ch>
2018-08-02 13:13:37 +0200

    Fix BluePyEfe install
    Change-Id: Ib159ebb1f6e83487e37c69f6fd40151c2d6c60f4

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #from distutils.core import setup
-import setuptools
 import subprocess
+
+import setuptools
 
 setuptools.setup(
     name='BluePyEfe',
     version='0.1.1dev',
     description='bluepyefe',
     packages=setuptools.find_packages(),
-    install_requires=['igorpy','matplotlib','efel','sh'],
-    dependency_links=['git+ssh://bbpcode.epfl.ch/user/vangeit/igorpy.git#egg=igorpy'],
+    install_requires=['igorpy', 'neo', 'matplotlib', 'efel', 'sh'],
+    dependency_links=['git+ssh://bbpcode.epfl.ch/user/vangeit/igorpy.git#egg=igorpy-1.0'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
