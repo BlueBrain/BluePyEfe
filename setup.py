4eef58f2bbaacce67273b49c51b8b9030b4d186c
Werner Van Geit <werner.vangeit@epfl.ch>
2019-08-23 14:48:24 +0200

    Clean up code a bit

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-#from distutils.core import setup
-import subprocess
-
 import setuptools
 
 setuptools.setup(
-    name='BluePyEfe',
-    version='0.1.1dev',
-    description='bluepyefe',
+    name='BluePyEfe', version='0.1.1dev', description='bluepyefe',
     packages=setuptools.find_packages(),
     install_requires=['igorpy', 'neo', 'matplotlib', 'efel', 'sh'],
-    dependency_links=['git+ssh://bbpcode.epfl.ch/user/vangeit/igorpy.git#egg=igorpy-1.0'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-)
+    dependency_links=[
+        'git+ssh://bbpcode.epfl.ch/user/vangeit/igorpy.git#egg=igorpy-1.0'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',)
