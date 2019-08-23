861555055480697fe318fe01debf00b2b0cc318d
Werner Van Geit <werner.vangeit@epfl.ch>
2019-08-23 16:22:01 +0200

    Add jenkins file

---
+++ .jenkins.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git config --global http.proxy http://bbpproxy.epfl.ch:80/
+git config --global https.proxy http://bbpproxy.epfl.ch:80/
+
+tox_args='-v --recreate'
+
+if [ "${os}" = "bb5" ]
+then
+	. /opt/rh/rh-python36/enable
+    tox_args="${tox_args} -e py3-functional"
+elif  [ "${os}" = "Ubuntu-18.04" ]
+then
+    tox_args="${tox_args} -e py3-functional"
+else
+    tox_args="${tox_args} -e py27-functional"
+fi
+
+cd $WORKSPACE
+
+#########
+# Virtualenv
+#########
+
+if [ ! -d "${WORKSPACE}/env" ]; then
+  virtualenv ${WORKSPACE}/env --no-site-packages
+fi
+
+. ${WORKSPACE}/env/bin/activate
+pip install pip --upgrade
+pip install tox --upgrade   
+
+
+#####
+# Tests
+#####
+
+cd  ${WORKSPACE}/BluePyEfe
+
+tox ${tox_args}
