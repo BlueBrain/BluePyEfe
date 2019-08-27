d8d73c7544d00ad6f7aff348afbbe8e6508bfe24
Werner Van Geit <werner.vangeit@epfl.ch>
2019-08-27 15:49:08 +0200

    Use pytest test generation

--- bluepyefe/tests/test_configs.py
+++ bluepyefe/tests/test_configs.py
@@ -2,47 +2,48 @@ import os
 import json
 import glob
 
-import pytest
-
 import bluepyefe as bpefe
 
 
-@pytest.fixture
-def rootdir():
-    return os.path.dirname(os.path.abspath(__file__))
+def pytest_generate_tests(metafunc):
+    """Generate config tests"""
 
-
-def test_config(rootdir):
-    """Test config"""
-
+    rootdir = os.path.dirname(os.path.abspath(__file__))
     configs_dir = os.path.join(rootdir, 'configs')
 
     config_paths = glob.glob(os.path.join(configs_dir, '*.json'))
 
-    for config_path in config_paths:
-        config = json.load(open(config_path))
+    metafunc.parametrize("rootdir,config_path",
+                         ((rootdir, config_path)
+                          for config_path in config_paths), ids=config_paths)
 
-        config['path'] = os.path.join(rootdir, config['path'])
 
-        '''
-        extractor = bpefe.Extractor('testtype_legacy', config, use_git=False)
-        extractor.create_dataset()
-        extractor.plt_traces()
-        extractor.extract_features(threshold=-30)
-        extractor.mean_features()
-        extractor.analyse_threshold()
-        extractor.plt_features()
-        extractor.feature_config_cells(version='legacy')
-        extractor.feature_config_all(version='legacy')
-        '''
+def test_config(rootdir, config_path):
+    """Test config"""
 
-        extractor = bpefe.Extractor('testtype', config, use_git=False)
-        extractor.create_dataset()
-        extractor.plt_traces()
-        extractor.extract_features(threshold=-30)
-        extractor.mean_features()
-        # extractor.analyse_threshold()
-        extractor.plt_features()
-        extractor.feature_config_cells()
-        extractor.feature_config_all()
-        extractor.plt_features_dist()
+    config = json.load(open(config_path))
+
+    config['path'] = os.path.join(rootdir, config['path'])
+
+    extractor = bpefe.Extractor('test_run', config, use_git=False)
+    extractor.create_dataset()
+    extractor.plt_traces()
+    extractor.extract_features(threshold=-30)
+    extractor.mean_features()
+    # extractor.analyse_threshold()
+    extractor.plt_features()
+    extractor.feature_config_cells()
+    extractor.feature_config_all()
+    extractor.plt_features_dist()
+
+    '''
+    extractor = bpefe.Extractor('testtype_legacy', config, use_git=False)
+    extractor.create_dataset()
+    extractor.plt_traces()
+    extractor.extract_features(threshold=-30)
+    extractor.mean_features()
+    extractor.analyse_threshold()
+    extractor.plt_features()
+    extractor.feature_config_cells(version='legacy')
+    extractor.feature_config_all(version='legacy')
+    '''
