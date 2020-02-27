b8716c7c403e555fde24ab936a16438615c1a702
Damart Tanguy Pierre Louis <damart@bb-c02xw077jhd5.epfl.ch>
2020-02-27 12:58:07 +0100

    Added numpy to setup to avoid build error with pandas

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     description='bluepyefe',
     packages=setuptools.find_packages(),
     install_requires=[
+        'numpy',
         'igor',
         'neo',
         'matplotlib',
