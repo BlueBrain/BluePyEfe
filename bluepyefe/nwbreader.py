608684fb3f21e3d11405eb98876feb8e468a5293
darshanmandge <darshan.mandge@epfl.ch>
2024-06-10 16:29:28 +0200

    lint fix

--- bluepyefe/nwbreader.py
+++ bluepyefe/nwbreader.py
@@ -115,9 +115,8 @@ class ScalaNWBReader(NWBReader):
         for sweep in list(self.content['acquisition'].keys()):
             key_current = sweep.replace('Series', 'StimulusSeries')
             protocol_name = self.content["acquisition"][sweep].attrs["stimulus_description"]
-            
-            if ("na"==protocol_name.lower()) or ("step" in protocol_name.lower()):
-                protocol_name ="Step"
+            if ("na" == protocol_name.lower()) or ("step" in protocol_name.lower()):
+                protocol_name = "Step"
 
             if (
                 self.target_protocols and
